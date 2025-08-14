a0b533c2683f635d1d2415ab7808fe1c5e19b595
Alvaro Vergara Cortes <alvaro.vergara.cl@gmail.com>
2025-08-14 00:40:07 -0400

    Version inicial

---
+++ src/main/java/cl/bci/common/annotation/PasswordPattern.java
@@ -0,0 +1,20 @@
+package cl.bci.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordPattern {
+	String message() default "{password.message.invalid}";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
